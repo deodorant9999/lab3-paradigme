4e4466051df704d5a52dd1ddb5466ecbd5677b02
deodorant9999@gmail.com <deodorant9999@gmail.com>
2024-03-19 09:53:55 +0200

    lab3 parola

--- src/main/java/ro/ulbs/paradigme/lab3/util/PasswordMaker.java
+++ src/main/java/ro/ulbs/paradigme/lab3/util/PasswordMaker.java
@@ -1,10 +1,36 @@
 package ro.ulbs.paradigme.lab3.util;
 
-import RandomStringGenerator
+
 
 public class PasswordMaker {
-    static final int MAGIC_NUMBER;
-    static final String MAGIC_STRING
-
-
+    private String name;
+    private String password="";
+    public PasswordMaker(String name)
+    {
+        this.name=name;
+    }
+    static final int MAGIC_NUMBER=23;
+    static final String MAGIC_STRING=RandomStringGenerator(MAGIC_NUMBER);
+    public static String RandomStringGenerator(int length)
+    {
+        int st=97;
+        int dr=122;
+        String randomString="";
+        for(int i=0;i<length;i++)
+        {
+            char c=(char)(st+Math.random()*(dr-st));
+            randomString=randomString+c;
+        }
+        return randomString;
+    }
+    public String generatePassword()
+    {
+        String s="";
+        for(int i=0;i<10;i++)
+        {
+            s=s+MAGIC_STRING.charAt((int)(Math.random()*MAGIC_NUMBER));
+        }
+        password=password+MAGIC_STRING+s+String.valueOf(name.length())+(int)(Math.random()*51);
+        return password;
+    }
 }
