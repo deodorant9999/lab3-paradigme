c5181edb41025f9ab2dd52f8bf59b959a3f6a040
deodorant9999@gmail.com <deodorant9999@gmail.com>
2024-03-19 08:44:13 +0200

    lab3

---
+++ src/main/java/ro/ulbs/paradigme/lab3/util/PasswordMaker.java
@@ -0,0 +1,10 @@
+package ro.ulbs.paradigme.lab3.util;
+
+import RandomStringGenerator
+
+public class PasswordMaker {
+    static final int MAGIC_NUMBER;
+    static final String MAGIC_STRING
+
+
+}
